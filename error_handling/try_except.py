3bb91621b14e31a21b0091a7fd8848f788a7601a
HemanthKumar M <hemanthkumarava@gmail.com>
2025-08-26 10:43:43 +0530

    Added Try Except

---
+++ error_handling/try_except.py
@@ -0,0 +1,10 @@
+try:
+    a = int(input("Enter a Number 1 :"))
+    b = int(input("Enter a Number 2 :"))
+    print("Division of A & B is :", a / b)
+except Exception as abc:
+    print(str(abc))
+
+a = 10
+b = 20
+print(a + b)
